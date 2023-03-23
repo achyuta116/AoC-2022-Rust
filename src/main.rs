36544406be8036df2c84bf2206791ef9d6e3434f
Achyuta <achyutathegreat@gmail.com>
2023-03-23 21:53:24 +0530

    AoC 3-1

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,43 @@
 use std::{
     fs::File,
     io::{self, BufRead},
-    path::Path,
+    path::Path, collections::HashSet,
 };
 
 fn main() {
     let path = Path::new("./input.txt");
-    let display = path.display();
-    println!("{}", display);
     let file = match File::open(&path) {
         Ok(file) => file,
         Err(w) => panic!("{}", w),
     };
 
-    let mut score = 0;
+
+    let mut sum_priorities = 0;
 
     for line in io::BufReader::new(file).lines() {
-        if let Ok(ip) = line {
-            let choices = ip.split_once(" ");
-            if let Some((a, b)) = choices {
-                score += calculate_score(a, b);
+        let mut first_set: HashSet<char> = HashSet::new();
+        let mut second_set: HashSet<char> = HashSet::new();
+        let line = line.unwrap();
+        for (index, char) in line.chars().enumerate() {
+            if index < line.len()/2 {
+                second_set.insert(char);
+                continue
             }
+            first_set.insert(char);
+        }
+        let intersection = first_set.intersection(&second_set);
+        for char in intersection.into_iter() {
+            sum_priorities += get_value(char);
         }
     }
 
-    println!("{}", score);
+    println!("{}", sum_priorities);
 }
 
-fn calculate_score(a: &str, b: &str) -> i32 {
-    match (a, b) {
-        ("A", "X") => 0 + 3,
-        ("A", "Y") => 3 + 1,
-        ("A", "Z") => 6 + 2,
-        ("B", "X") => 0 + 1,
-        ("B", "Y") => 3 + 2,
-        ("B", "Z") => 6 + 3,
-        ("C", "X") => 0 + 2,
-        ("C", "Y") => 3 + 3,
-        ("C", "Z") => 6 + 1,
-        _ => panic!("Incorrect Combination"),
+fn get_value(char: &char) -> u32 {
+    match char {
+        'A'..='Z' => char.to_owned() as u32 - 'A' as u32 + 27,
+        'a'..='z' => char.to_owned() as u32 - 'a' as u32 + 1,
+        _ => panic!("Unexpected Character")
     }
 }
