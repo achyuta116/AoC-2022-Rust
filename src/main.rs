14bbb7f2d0a2e6c61eea16fda783b65c1de4f0f9
Achyuta <achyutathegreat@gmail.com>
2023-03-27 00:39:03 +0530

    AoC 8-2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::cmp::max;
 use std::error::Error;
 use std::fs::read_to_string;
 
@@ -12,54 +12,63 @@ fn main() -> Result<(), Box<dyn Error>> {
         trees.push(treeline);
     }
 
-    let mut visible: HashSet<(u32, u32)> = HashSet::new();
-
+    let mut mx = 1;
     // l to r
-    for i in 0..trees.len() {
-        visible.insert((i as u32, 0));
-        let mut mx = trees[i][0].to_digit(10).unwrap();
-        for j in 1..trees[0].len() {
-            if trees[i][j].to_digit(10).unwrap() > mx {
-                mx = trees[i][j].to_digit(10).unwrap();
-                visible.insert((i as u32, j as u32));
+    for i in 1..trees.len() - 1 {
+        for j in 1..trees[0].len() - 1 {
+            let mut sc = 1;
+            let mut sc_mul = 0;
+            let ht = trees[i][j].to_digit(10).unwrap();
+
+
+            for k in j + 1..trees.len() {
+                if trees[i][k].to_digit(10).unwrap() < ht {
+                    sc_mul += 1;
+                } else {
+                    sc_mul += 1;
+                    break
+                }
             }
-        }
-    }
-    // r to l
-    for i in 0..trees.len() {
-        visible.insert((i as u32, (trees.len() - 1) as u32));
-        let mut mx = trees[i][trees.len() - 1].to_digit(10).unwrap();
-        for j in (0..trees[0].len() - 1).rev() {
-            if trees[i][j].to_digit(10).unwrap() > mx {
-                mx = trees[i][j].to_digit(10).unwrap();
-                visible.insert((i as u32, j as u32));
+            sc *= sc_mul;
+
+            sc_mul = 0;
+            for k in (0..=j - 1).rev() {
+                if trees[i][k].to_digit(10).unwrap() < ht {
+                    sc_mul += 1;
+                } else {
+                    sc_mul += 1;
+                    break
+                }
             }
-        }
-    }
-    // u to d
-    for j in 0..trees[0].len() {
-        visible.insert((0, j as u32));
-        let mut mx = trees[0][j].to_digit(10).unwrap();
-        for i in 1..trees.len() {
-            if trees[i][j].to_digit(10).unwrap() > mx {
-                mx = trees[i][j].to_digit(10).unwrap();
-                visible.insert((i as u32, j as u32));
+            sc *= sc_mul;
+
+            sc_mul = 0;
+            for k in i + 1..trees.len() {
+                if trees[k][j].to_digit(10).unwrap() < ht {
+                    sc_mul += 1;
+                } else {
+                    sc_mul += 1;
+                    break
+                }
             }
-        }
-    }
-    // d to u
-    for j in 0..trees.len() {
-        visible.insert(((trees.len() - 1) as u32, j as u32));
-        let mut mx = trees[trees.len() - 1][j].to_digit(10).unwrap();
-        for i in (0..trees[0].len()-1).rev() {
-            if trees[i][j].to_digit(10).unwrap() > mx {
-                mx = trees[i][j].to_digit(10).unwrap();
-                visible.insert((i as u32, j as u32));
+            sc *= sc_mul;
+
+            sc_mul = 0;
+            for k in (0..=i - 1).rev() {
+                if trees[k][j].to_digit(10).unwrap() < ht {
+                    sc_mul += 1;
+                } else {
+                    sc_mul += 1;
+                    break
+                }
             }
+            sc *= sc_mul;
+
+            mx = max(mx, sc);
         }
     }
 
-    println!("{}", visible.len());
+    println!("{}", mx);
 
     Ok(())
 }
