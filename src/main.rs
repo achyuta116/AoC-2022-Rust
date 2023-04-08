ff9aaedad20e6ed73dba1e9f2f7a7350baab241d
Achyuta <achyutathegreat@gmail.com>
2023-04-08 09:18:57 +0530

    gradient ascii

--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,40 @@
 use std::error::Error;
-use std::fs::read_to_string;
 
-#[derive(Debug)]
-struct Monkey {
-    items: Vec<i32>,
-    operation: char,
-    operand: Option<i32>,
-    modulus: i32,
-    on_true: usize,
-    on_false: usize,
-    inspection: i32,
-}
+struct Color(i16, i16, i16);
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = read_to_string("input.txt").unwrap();
-    let mut input = input.lines();
-    let mut monkeys = vec![];
+    let fl = include_str!("base.txt");
 
-    let mut mod_constant = 1;
+    let fc = Color(255, 255, 0);
+    let bc = Color(0, 255, 100);
 
-    while let Some(mut line) = input.next() {
-        line = line.strip_prefix("Monkey ").unwrap();
-        line = line.strip_suffix(":").unwrap();
-        let _ = line.parse::<i32>().unwrap();
+    let mut line_count = 0;
+    fl.lines().for_each(|_| line_count += 1);
+    let ln = fl.lines().max_by(|x, y| x.len().cmp(&y.len())).unwrap().len();
 
-        let mut line = input.next().unwrap();
-        let items = line
-            .trim()
-            .strip_prefix("Starting items: ")
-            .unwrap()
-            .split(", ")
-            .collect::<Vec<_>>();
-        let items = items.iter().map(|&x| x.parse::<i32>().unwrap()).collect();
-        line = input.next().unwrap();
-        line = line.trim().strip_prefix("Operation: new = old ").unwrap();
-        let (first, last) = line.split_once(" ").unwrap();
-
-        line = input.next().unwrap();
-        let modulus = line
-            .trim()
-            .strip_prefix("Test: divisible by ")
-            .unwrap()
-            .parse::<i32>()
-            .unwrap();
-
-        line = input.next().unwrap();
-        let on_true = line
-            .trim()
-            .strip_prefix("If true: throw to monkey ")
-            .unwrap()
-            .parse::<usize>()
-            .unwrap();
-
-        line = input.next().unwrap();
-
-        let on_false = line
-            .trim()
-            .strip_prefix("If false: throw to monkey ")
-            .unwrap()
-            .parse::<usize>()
-            .unwrap();
-
-        mod_constant *= modulus;
-
-        monkeys.push(Monkey {
-            items,
-            operation: first.chars().next().unwrap(),
-            modulus,
-            operand: match last.parse() {
-                Ok(i) => Some(i),
-                Err(_) => None,
-            },
-            on_true,
-            on_false,
-            inspection: 0
-        });
-
-        input.next();
-    }
-
-
-    for _ in 0..10000 {
-        for i in 0..monkeys.len() {
-            while let Some(item) = monkeys[i].items.pop() {
-                monkeys[i].inspection += 1;
-                let operand = match monkeys[i].operand {
-                    Some(value) => value,
-                    None => item,
-                };
-
-                let modulus = monkeys[i].modulus;
-
-                let new = match monkeys[i].operation {
-                    '*' => (item as i64 * operand as i64) % mod_constant as i64,
-                    '+' => (item as i64 + operand as i64) % mod_constant as i64,
-                    _ => panic!("Incorrect Operation")
-                } as i32;
-
-                if new % modulus  == 0 {
-                    let throw = monkeys[i].on_true;
-                    monkeys[throw].items.push(new);
-                } else {
-                    let throw = monkeys[i].on_false;
-                    monkeys[throw].items.push(new);
-                }
-            }
+    let mut colored = vec![];
+    let mut input = fl.lines();
+    let mut line_no = 0;
+    while let Some(line) = input.next() {
+        let mut tmp = vec![];
+        for (index, ch) in line.chars().enumerate() {
+            let percent =
+                (index as f64).hypot(line_no as f64) / (line_count as f64).hypot(ln as f64);
+            let r: u8 = (fc.0 as f64 + percent * (bc.0 - fc.0) as f64) as u8;
+            let g: u8 = (fc.1 as f64 + percent * (bc.1 - fc.1) as f64) as u8;
+            let b: u8 = (fc.2 as f64 + percent * (bc.2 - fc.2) as f64) as u8;
+            tmp.push(format!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, ch));
         }
+        colored.push(tmp);
+        line_no += 1;
     }
-    
-    let mut inspections = monkeys.iter().map(|x| x.inspection).collect::<Vec<_>>();
-    inspections.sort();
-    inspections.reverse();
-    println!("{}", inspections[0] as i64 * inspections[1] as i64);
+
+    for v in colored {
+        for s in v {
+            print!("{}", s);
+        }
+        println!();
+    }
 
     Ok(())
 }
